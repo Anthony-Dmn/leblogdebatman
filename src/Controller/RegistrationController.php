b43d24f7e2bcf52ed5eb01cc27cd8041fbe75a8e
AnthonyD <a@a.a>
2023-01-11 12:28:11 +0100

    Ajout captcha page inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Recaptcha\RecaptchaValidator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -19,7 +21,7 @@ class RegistrationController extends AbstractController
      * Contrôleur de la page d'inscription
      */
     #[Route('/creer-un-compte/', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, RecaptchaValidator $recaptcha): Response
     {
 
         // Si l'utilisateur est déjà connecté, on le redirige de force sur la page d'accueil du site
@@ -36,29 +38,47 @@ class RegistrationController extends AbstractController
         // Remplissage du formulaire avec les données POST (qui sont $request)
         $form->handleRequest($request);
 
-        // Si le formulaire a bien été envoyé et ne possède pas d'erreur
-        if ($form->isSubmitted() && $form->isValid()) {
+        // Si le formulaire a bien été envoyé
+        if ($form->isSubmitted()) {
 
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+            // Récupération de la valeur du captcha ( $_POST['g-recaptcha-response'] )
+            $captchaResponse = $request->request->get('g-recaptcha-response', null);
 
-            // Hydratation de la date d'inscription du nouvel utilisateur
-            $user->setRegistrationDate( new \DateTime() );
+            // Récupération de l'adresse IP de l'utilisateur ( $_SERVER['REMOTE_ADDR'] )
+            $ip = $request->server->get('REMOTE_ADDR');
 
-            // Sauvegarde du nouvel utilisateur en BDD
-            $entityManager->persist($user);
-            $entityManager->flush();
+            // Si le captcha est null ou s'il est invalide, on ajoute une erreur dans le formulaire
+            if( $captchaResponse == null || !$recaptcha->verify($captchaResponse, $ip) ){
 
-            // Message flash de succès
-            $this->addFlash('success', 'Votre compte a bien été créé avec succès !');
+                $form->addError( new FormError('Veuillez remplir le captcha de sécurité') );
 
-            // Redirection de l'utilisateur vers la page de connexion
-            return $this->redirectToRoute('app_login');
+            }
+
+            if($form->isValid()){
+
+                // encode the plain password
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+
+                // Hydratation de la date d'inscription du nouvel utilisateur
+                $user->setRegistrationDate( new \DateTime() );
+
+                // Sauvegarde du nouvel utilisateur en BDD
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                // Message flash de succès
+                $this->addFlash('success', 'Votre compte a bien été créé avec succès !');
+
+                // Redirection de l'utilisateur vers la page de connexion
+                return $this->redirectToRoute('app_login');
+
+            }
+
         }
 
         return $this->render('registration/register.html.twig', [
---
+++ src/Recaptcha/RecaptchaValidator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Recaptcha;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+/**
+ * Service permettant de vérifier si un captcha Google est valide
+ */
+class RecaptchaValidator{
+
+    private ParameterBagInterface $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
+
+    public function verify(?string $recaptchaResponse, ?string $ip = null): bool
+    {
+
+        if(empty($recaptchaResponse)) {
+            return false;
+        }
+        $params = [
+            'secret'    => $this->params->get('google_recaptcha_private_key'),
+            'response'  => $recaptchaResponse
+        ];
+        if($ip){
+            $params['remoteip'] = $ip;
+        }
+        $url = "https://www.google.com/recaptcha/api/siteverify?" . http_build_query($params);
+        if(function_exists('curl_version')){
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($curl);
+        }else{
+            $response = file_get_contents($url);
+        }
+        if(empty($response) || is_null($response)){
+            return false;
+        }
+        $json = json_decode($response);
+        return $json->success;
+
+    }
+
+}
